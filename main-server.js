fa4dfd68dcb3b1c5aba715c6ae792ff669c1cc3f
gessex22 <gessembergsteiner@gmail.com>
2024-04-24 20:57:32 -0500

    Se solucionaron errores, y e actualizo la GUI

--- main-server.js
+++ main-server.js
@@ -8,8 +8,6 @@ const { log } = require("console");
 const storage = multer.diskStorage({
   destination: "uploads/",
   filename: function (req, file, cb) {
-    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    // const fileExtension = path.extname(file.originalname);
     const fileName = file.originalname;
     cb(null, fileName);
   },
@@ -18,7 +16,12 @@ const upload = multer({ storage });
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-app.use(express.static("public"));
+
+app.use(
+  express.static(
+    "./Documents/Documents/proyectos/server-multimedia-wyrruz/public"
+  )
+);
 
 app.post("/upload", upload.single("files"), (req, res) => {
   const uploadedFile = req.file;
@@ -35,19 +38,18 @@ app.post("/upload", upload.single("files"), (req, res) => {
   // Manejar el evento 'data' para obtener el progreso de subida
 
   //const ass = setInterval(()=>{
-    readStream.on("data", (chunk) => {
-      uploadedSize += chunk.length;
-  
-      // Calcular el progreso actual en porcentaje
-      let progress = Math.floor((uploadedSize / size) * 100);
-      console.log(progress)
-      console.log(chunk)
-      // Emitir el progreso a través de Socket.IO
-      io.emit("progressUpdate", progress);
-    });
+  readStream.on("data", (chunk) => {
+    uploadedSize += chunk.length;
+
+    // Calcular el progreso actual en porcentaje
+    let progress = Math.floor((uploadedSize / size) * 100);
+    console.log(progress);
+    console.log(chunk);
+    // Emitir el progreso a través de Socket.IO
+    io.emit("progressUpdate", progress);
+  });
 
   //},100)
-  
 
   // Manejar el evento 'end' para finalizar la subida
   readStream.on("end", () => {
@@ -55,7 +57,7 @@ app.post("/upload", upload.single("files"), (req, res) => {
 
     // Emitir evento de finalización a través de Socket.IO
     io.emit("uploadComplete");
-    clearInterval(ass)
+    //clearInterval(ass) --esto lo anule---------------
 
     // Enviar respuesta al cliente
     res.status(200).json({ message: "Archivo subido exitosamente" });
@@ -70,11 +72,11 @@ io.on("connection", (socket) => {
   });
 });
 
-io.on('getProgress', () => {
+io.on("getProgress", () => {
   // Aquí puedes obtener el progreso actual de la carga y enviarlo al cliente
   // Por ejemplo, puedes obtener el progreso de una variable global o desde una base de datos
-  const progress = 50; 
-  socket.emit('progressUpdate', progress);
+  const progress = 50;
+  socket.emit("progressUpdate", progress);
 });
 
 server.listen(3000, () => {
